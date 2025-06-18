82a2d63d9ac0decfbe6e538a6401063c3c1a6655
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-06-18 10:13:04 +0300

    fixed utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import json
 from typing import Any, Dict, List, cast
 
+
 def read_json(path: Any) -> list[dict[Any, Any]]:
     """Функция для чтения json-файла"""
     try:
@@ -8,4 +9,3 @@ def read_json(path: Any) -> list[dict[Any, Any]]:
             return cast(List[Dict[Any, Any]], json.load(f))
     except (FileNotFoundError, json.JSONDecodeError):
         return []
-    
