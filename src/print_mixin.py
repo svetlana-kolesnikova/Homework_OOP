b38d94dc23a878a0fac3e72d533fefd183ee426a
Svetlana Kolesnikova <sv.ps1286@gmail.com>
2025-06-29 13:40:01 +0300

    add print_mixin.py

---
+++ src/print_mixin.py
@@ -0,0 +1,10 @@
+class PrintMixin:
+    """Класс-миксин для вывода информации о параметрах создаваемого объекта"""
+
+    def __init__(self) -> None:
+        """Метод для инициализации экземпляра класса"""
+        print(repr(self))
+
+    def __repr__(self) -> str:
+        """печатать в консоль информацию о том, от какого класса и с какими параметрами был создан объект"""
+        return f'{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})'
